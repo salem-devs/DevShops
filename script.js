9ce890e9ebdae965bd75db603cc0c0059451c7ba
Ben-Salem <salemko801@gmail.com>
2026-08-15 23:54:02 +0100

    integration de script

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiUrl = "https://fakestoreapi.com/products";
+
+const productsContainer = document.getElementById("productsContainer");
+const loader = document.getElementById("loader");
+const errorMessage = document.getElementById("errorMessage");
+
+let products = [];
