b2829f84862f67c12bc3cfcfd09e8f68dfe76833
Ben-Salem <salemko801@gmail.com>
2026-08-15 23:59:51 +0100

    récupérer les produits et gestion d'erreurs

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ const loader = document.getElementById("loader");
 const errorMessage = document.getElementById("errorMessage");
 
 let products = [];
+
+// Récupéreration des produits
+async function getProducts() {
+    try {
+        loader.style.display = "block";
+
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error("Erreur lors de la récupération des produits");
+        }
+
+        const data = await response.json();
+
+        products = data;
+
+        console.log(products);
+
+    } catch (error) {
+        console.error(error);
+        errorMessage.textContent = "Impossible de charger les produits.";
+
+    } finally {
+        loader.style.display = "none";
+    }
+}
+
+
